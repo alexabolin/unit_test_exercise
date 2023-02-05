9dc12b108bcf6ac9958e060b46bb18f3018354b7
Alexa Bolin <anb71@duke.edu>
2023-02-05 17:06:50 -0500

    altered the name of the test function to match the function being tested

--- test_plane_line.py
+++ test_plane_line.py
@@ -11,7 +11,7 @@ import pytest
                          [((0, 0), (1, 1), 0.5, 0.5),
                           ((-1, -2), (1, 2), 0, 0),
                           ((1, 4), (5, 6), 3, 5)])
-def test_plan_line(plane_input_1, plane_input_2, new_x, expected):
+def test_new_y(plane_input_1, plane_input_2, new_x, expected):
     from plane_line import new_y
     # Arrange (parameters)
     # Act
